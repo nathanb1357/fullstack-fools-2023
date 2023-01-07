a2baabcc787dd985dec894645bcf56218f1d8ec8
Eric Guo <yguo91@my.bcit.ca>
2023-01-07 13:35:36 -0800

    add some works on mysql in server side

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ const app = express();
 app.use(express.json());
 const fs = require("fs");
 
+var colomnID = 1;
+
 // just like a simple web server like Apache web server
 // we are mapping file system paths to the app's virtual paths
 app.use("/js", express.static("./public/js"));
 app.use("/css", express.static("./public/css"));
 app.use("/img", express.static("./public/img"));
-
 
 app.get("/", function (req, res) {
     let doc = fs.readFileSync("./app/html/index.html", "utf8");
@@ -24,11 +25,45 @@ app.get("/", function (req, res) {
     res.send(doc);
 });
 
+// app.get("/", function (req, res) {
+    
+//     const prasedData = JSON.parse(data);
+
+//     const trucks = prasedData.TRUCK_ID;
+
+//     console.log(trucks);
+
+//     // just send the text stream
+//     res.send(doc);
+// });
+
 
 //=====================================================================
 //our real code from here
 
+//async way
+async function readMySQL(res) {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "fvdata", //currently fvdata is the data base name
+        multipleStatements: true
+    });
+    connection.connect();
+    const queryState = "SELECT * FROM `fruits` ORDER BY = ?"; //fruits is the table name 
+    
+    const [rows, fields] = await connection.execute(queryState, [colomnID]);
 
+    //=====================================
+    //feedback info as table or else
+    //=====================================
+    
+    connection.end();
+    res.send(table);
+    return;
+}
 
 
 
